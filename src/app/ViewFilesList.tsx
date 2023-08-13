8cf62daa8658e884fd7ae9d7886e2357b538cca5
YOKOYAMA Taku <seekseep@gmail.com>
2023-08-13 15:45:22 +0900

    update ui

--- src/app/ViewFilesList.tsx
+++ src/app/ViewFilesList.tsx
@@ -3,10 +3,22 @@
 import {
   useTheme,
   Heading, Flex, Alert, View,
-  Text, Divider
+  Text, Divider, Button
 } from "@aws-amplify/ui-react"
 import { useQuery } from "@tanstack/react-query"
 import { Storage } from "@aws-amplify/storage"
+
+function formatBytes(bytes:number, decimals = 2) {
+  if (bytes === 0) return '0 Bytes';
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
 
 const ViewFilesList = () => {
   const theme = useTheme()
@@ -31,15 +43,16 @@ const ViewFilesList = () => {
     }
   })
 
-  console.log(query.data)
-
   return (
     <Flex
       direction="column"
-      maxWidth={theme.breakpoints.values.medium}
+      maxWidth={theme.breakpoints.values.large}
       padding={theme.tokens.space.medium}
       gap={theme.tokens.space.medium}>
-      <Heading level={1}>View Files List</Heading>
+      <Flex alignItems="center">
+        <Heading grow={1} level={1}>View Files List</Heading>
+        <Button onClick={() => query.refetch() }>Reload</Button>
+      </Flex>
       {query.isError && (
         <Alert variation="error" title="Failed to Fetch Files List">{String(query.error)}</Alert>
       )}
@@ -47,9 +60,12 @@ const ViewFilesList = () => {
         <>
           {query.data.map(item => (
             <View key={item.key}>
-              <Flex gap={theme.tokens.space.xs} paddingBlock={theme.tokens.space.xxs}>
-                <Text>{item.key}</Text>
-                <Text>{item.size}</Text>
+              <Flex gap={theme.tokens.space.small} paddingBlock={theme.tokens.space.xs}>
+                <Text grow={1}>{item.key}</Text>
+                <Text textAlign="end" width={128} shrink={0}>
+                  {typeof item.size == "number" ? formatBytes(item.size) : ''}
+                </Text>
+                <Text>{item.lastModified?.toISOString()}</Text>
               </Flex>
               <Divider />
             </View>
