606b3f6b4df1e8ec731e1a00c62e5f69063e7a44
YOKOYAMA Taku <seekseep@gmail.com>
2023-08-13 15:33:11 +0900

    update token generator

--- amplify/backend/function/amplifystoragegroupp409e85e5PreTokenGeneration/src/alter-claims.js
+++ amplify/backend/function/amplifystoragegroupp409e85e5PreTokenGeneration/src/alter-claims.js
@@ -2,6 +2,18 @@
  * @type {import('@types/aws-lambda').PreTokenGenerationTriggerHandler}
  */
 exports.handler = async (event) => {
+  const groupConfiguration  = event.request.groupConfiguration
+
+  /** @type {string[]} */
+  const groupsToOverride = groupConfiguration.groupsToOverride ?? []
+  /** @type {string[]} */
+  const iamRolesToOverride = groupConfiguration.iamRolesToOverride ?? []
+  /** @type {string[]} */
+  const preferredRole = groupConfiguration.preferredRole ?? []
+
+  // NOTE: グループの追加
+  groupsToOverride.push(...['additional-group-1', 'additional-group-2'])
+
   event.response = {
     claimsOverrideDetails: {
       claimsToAddOrOverride: {
@@ -9,10 +21,9 @@ exports.handler = async (event) => {
         attribute_key2: 'attribute_value2',
       },
       groupOverrideDetails: {
-        groupsToOverride: [
-          ...event.request.groupConfiguration.groupsToOverride,
-          // 'hoge'
-        ]
+        groupsToOverride: groupsToOverride,
+        iamRolesToOverride: iamRolesToOverride,
+        preferredRole: preferredRole,
       },
       claimsToSuppress: ['attribute_key3'],
     },
