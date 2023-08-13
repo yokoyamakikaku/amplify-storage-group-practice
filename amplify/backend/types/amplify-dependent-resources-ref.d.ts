0135a4048120a3e048ae4faf6cc4dc67425ed180
YOKOYAMA Taku <seekseep@gmail.com>
2023-08-13 10:47:44 +0900

    update auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,8 +3,6 @@ export type AmplifyDependentResourcesAttributes = {
     "amplifystoragegroupp409e85e5": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
